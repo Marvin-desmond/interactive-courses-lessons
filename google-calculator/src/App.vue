b321ebe68ec463b2341d2be9663df26326eb9552
MarvinPesapal <marvin.mboya@pesapal.com>
2023-04-19 15:52:07 +0300

    google calculator starter codebase

---
+++ google-calculator/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div class="h-screen w-screen grid place-items-center bg-basic">
+    <h1 class="text-highlight">Google Calculator Starter</h1>
+  </div>
+</template>
